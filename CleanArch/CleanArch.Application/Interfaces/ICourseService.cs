55d0860a7341eca515b04c324cc9236862ada075
konaspr <konaspr@gmail.com>
2020-03-13 22:47:40 +0200

    added create functionality to Courses Service, updated CourseViewModel

--- CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
+++ CleanArch/CleanArch.Application/Interfaces/ICourseService.cs
@@ -8,5 +8,9 @@ namespace CleanArch.Application.Interfaces
     public interface ICourseService
     {
         CourseViewModel GetCourses();
+
+        void CreateCourse(CourseViewModel courseViewModel);
+
+
     }
 }
