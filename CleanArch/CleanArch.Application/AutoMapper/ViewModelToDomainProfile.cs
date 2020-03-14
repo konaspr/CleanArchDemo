8aa8a56d965b34fba3c053ee9b98af5e8254f397
konaspr <konaspr@gmail.com>
2020-03-14 16:44:03 +0200

    added AutoMapper

---
+++ CleanArch/CleanArch.Application/AutoMapper/ViewModelToDomainProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.AutoMapper
+{
+    public class ViewModelToDomainProfile : Profile
+    {
+        public ViewModelToDomainProfile()
+        {
+            CreateMap<CourseViewModel, CreateCourseCommand>()
+                .ConvertUsing(c => new CreateCourseCommand(c.Name, c.Description, c.ImageUrl));
+
+            CreateMap<CourseViewModel, UpdateCourseCommand>()
+                .ConvertUsing(c => new UpdateCourseCommand(c.Id, c.Name, c.Description, c.ImageUrl));
+        }
+    }
+}
--- CleanArch/CleanArch.Application/Services/CourseService.cs
+++ CleanArch/CleanArch.Application/Services/CourseService.cs
@@ -1,4 +1,6 @@
-﻿using CleanArch.Application.Interfaces;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModels;
 using CleanArch.Domain.Commands;
 using CleanArch.Domain.Core.Bus;
@@ -13,48 +15,34 @@ namespace CleanArch.Application.Services
     {
         private readonly ICourseRepository _courseRepository;
         private readonly IMediatorHandler _bus;
+        private readonly IMapper _autoMapper;
 
-        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus, IMapper autoMapper)
         {
-            _courseRepository = courseRepository;
             _bus = bus;
+            _autoMapper = autoMapper;
+            _courseRepository = courseRepository;
         }
 
         public void CreateCourse(CourseViewModel courseViewModel)
         {
-            var createCourseCommand = new CreateCourseCommand(
-                    courseViewModel.Name,
-                    courseViewModel.Description,
-                    courseViewModel.ImageUrl
-                );
-
-            _bus.SendCommand(createCourseCommand);
+            _bus.SendCommand(_autoMapper.Map<CreateCourseCommand>(courseViewModel));
         }
 
         public void UpdateCourse(CourseViewModel courseViewModel)
         {
-            var updateCourseCommand = new UpdateCourseCommand(
-                courseViewModel.Id,
-                courseViewModel.Name,
-                courseViewModel.Description,
-                courseViewModel.ImageUrl
-            );
-
-            _bus.SendCommand(updateCourseCommand);
+            _bus.SendCommand(_autoMapper.Map<UpdateCourseCommand>(courseViewModel));
         }
 
         public void DeleteCourse(int id)
         {
-            var deleteCourseCommand = new DeleteCourseCommand(id);
-            _bus.SendCommand(deleteCourseCommand);
+            _bus.SendCommand(new DeleteCourseCommand(id));
         }
 
-        public CourseViewModel GetCourses()
+        public IEnumerable<CourseViewModel> GetCourses()
         {
-            return new CourseViewModel()
-            {
-                Courses = _courseRepository.GetCourses()
-            };
+            return _courseRepository.GetCourses()
+                .ProjectTo<CourseViewModel>(_autoMapper.ConfigurationProvider);
         }
     }
 }
